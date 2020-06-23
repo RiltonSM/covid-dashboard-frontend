5bd5a6d5691b79c9532aeb1620ce8e21e346761c
Rilton Moreira <rilton_moreira@hotmail.com>
2020-06-23 18:09:54 -0300

    Alteração rota backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://covid-dashboard-backend-rilton.herokuapp.com/'
 });
 
 export default api;
